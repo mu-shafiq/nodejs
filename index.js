361eb7979035ec0e038ae13350a6ec4295a97583
Shafiq <mshafiq.cse@gmail.com>
2023-09-27 23:56:08 +0500

    socket

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
+const express = require('express');
 const http = require('http');
-const PORT = 3000;
+const socketIo = require('socket.io');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+// Create an Express app
+const app = express();
+const server = http.createServer(app);
+
+// Create a Socket.io instance and attach it to the server
+const io = socketIo(server);
+
+// Define a route to serve your HTML page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+// Define a connection event handler
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Define custom events for handling client-server communication
+  socket.on('chat message', (message) => {
+    console.log(`Message: ${message}`);
+    // Broadcast the message to all connected clients
+    io.emit('chat message', message);
+  });
+
+  // Handle disconnections
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+// Start the server on port 3000
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
